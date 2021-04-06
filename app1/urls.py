b9089a9fddba77765dd46498952c753c8f01d683
vishnupriya <vishnupriya413@gmail.com>
2021-04-06 10:22:42 +0530

    first commit

--- app1/urls.py
+++ app1/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from app2 import views
+from app1 import views
 app_name="app1"
 
 urlpatterns=[
     path('',views.index,name="index"),
     path('sample/',views.sample,name="sample"),
     path('sample_app1/',views.sample_view,name="sample_view"),
-    
+    path("<data>/",views.carry_data,name="carry_data"),
+    path("fact/<num>/",views.facto,name="facto"),
+    path("add/<num1>/<num2>",views.add,name="add"),
 
 
 
--- app1/views.py
+++ app1/views.py
@@ -11,3 +11,24 @@ def sample(request):
     
 def sample_view(request):
     return render(request,"sample_app1.html")
+
+def carry_data(request,data):
+    return HttpResponse(f'<h1>The Recieved data is {data}</h1>')
+
+def facto(request,num):
+    fact=1
+    for i in range(int(num),1,-1):
+        fact*=i
+    return HttpResponse(f'<h2>the factorial of {num} is {fact}</h2>')
+
+
+def add(request,num1,num2):
+    try:
+        num1=int(num1)
+    except:
+        num1=float(num1)
+    try:
+        num2=int(num2)
+    except:
+        num2=float(num2)
+    return HttpResponse(num1+num2)
